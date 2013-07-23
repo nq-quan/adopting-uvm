71d2b6b6dd3675d90726435f21da01e679904f6c
bhunter <brian.p.hunter@gmail.com>
2013-07-23 13:35:42 -0700

    Cleaner set-up for gogo.

--- dvtools/gogo_site.py
+++ dvtools/gogo_site.py
@@ -1,17 +1,27 @@
+###############
+# UVM Options
+
 # Select UVM_REV
 UVM_REV   = '1_1d'
 
-# PLI files
+# Special PLI files
 PLI_FILES = ['/nfs/cacadtools/springsoft/Verdi-201307/share/PLI/VCS/LINUX/novas.tab',
              '/nfs/cacadtools/springsoft/Verdi-201307/share/PLI/VCS/LINUX/pli.a']
 
+###############
+# How to build with VCS
 VCS_VERSION = 'H-2013.06'  # G-2012.09-SP1-1
 BUILD_TOOL = 'runmod -m synopsys-vcs_mx/%s vcs' % VCS_VERSION
 VCOMP_DIR  = 'sim/.vcomp'
 
+# VCS Build Options
 BUILD_OPTIONS = '-q -debug_pp -notice -timescale=1ns/1ps -sverilog +libext+.v+.sv'
 BUILD_OPTIONS += ' +define+UVM_NO_DEPRECATED+UVM_OBJECT_MUST_HAVE_CONSTRUCTOR'
 BUILD_OPTIONS += '  -CFLAGS -DVCS'
+
+###############
+# Various VCS Flags
+SIM_GUI = ' -gui'
 
 ###############
 # LSF Command
@@ -23,5 +33,7 @@ LSF_BUILD_LICS = '-l lic_cmp_vcs'
 # Simulation Licenses
 LSF_SIM_LICS = '-l lic_sim_vcs'
 
-# Additional Options
-LSF_OPTIONS = '-N test'
+################
+# How to Clean Up
+CLEAN_DIRS = ('sim', 'csrc')
+CLEAN_FILES = ('ucli.key', 'vc_hdrs.h')
--- verif/alutb/tb.py
+++ verif/alutb/tb.py
@@ -3,3 +3,6 @@ VKITS = ['cn', 'global', 'csr', 'ctx', 'alutb']
 
 # Add testbench flists
 FLISTS = ['alutb.flist', 'rtl.flist']
+
+# Testbench Top-level
+TB_TOP = 'alutb_tb_top'
