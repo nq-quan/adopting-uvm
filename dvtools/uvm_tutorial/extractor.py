5d7822cdd65382ac0420bf0341abbe5728e828b9
bhunter <brian.p.hunter@gmail.com>
2013-09-22 18:26:16 -0700

    Extract latest codes from Tutorial.

--- dvtools/uvm_tutorial/extractor.py
+++ dvtools/uvm_tutorial/extractor.py
@@ -21,7 +21,7 @@ Rexp2 = re.compile(r"\\\'9[34]")
 
 #######################################################################################
 def start_snipping(line):
-    return line.find("\\strokec6 {\\listtext\t1.\t}") == 0
+    return line.find("\\strokec3 {\\listtext\t1.\t}") == 0
 
 #######################################################################################
 def end_snipping(line):
@@ -75,47 +75,42 @@ def parse_rtf(file_name):
     code_file_name = None
     prepending = None
 
-    try:
-        file = open(file_name)
-    except:
-        Log.critical("Unable to open", file_name)
+    with open(file_name) as file:
+       for (line_num, line) in enumerate(file.readlines()):
+           if snipping:
+               # Log.debug("Here with line: '%s'" % line)
+               pass
 
-    for (line_num, line) in enumerate(file.readlines()):
-        if snipping:
-            # Log.debug("Here with line: '%s'" % line)
-            pass
+           if not snipping:
+               if start_snipping(line):
+                   code_num += 1
 
-        if not snipping:
-            if start_snipping(line):
-                code_num += 1
-
-                code_file_name = line[line.find('verif'):].rstrip().rstrip('\\')
-                Log.info("Found %d starting on line: %d with code_file_name: %s\n%s" % (code_num, line_num, code_file_name, line))
-                snipping = True
-                snip_lines = []
-        else:
-            if end_snipping(line):
-                snipping = False
-                codes[code_num] = (code_file_name, '\n'.join(snip_lines))
-                Log.debug("Closed", code_num, "with", len(snip_lines), "lines on line", line_num)
-            elif line == '\\\n':
-                snip_lines.append("")
-                Log.debug("Added an empty new line.")
-            elif line.isspace():
-                # prepend it to the next line printed
-                prepending = line[:-1]
-                Log.debug("Set prepending: '%s'" % prepending)
-            else:
-                new_line = consume_escapes(line)
-                Log.debug("New_line now = '%s'" % new_line)
-                if new_line:
-                    if prepending:
-                        new_line = prepending + new_line
-                        Log.debug("Prepended '%s'" % prepending)
-                        prepending = None
-                    snip_lines.append(new_line.rstrip())
-                    Log.debug("Added new_line: '%s'" % new_line)
-    file.close()
+                   code_file_name = line[line.find('verif'):].rstrip().rstrip('\\')
+                   Log.info("Found %d starting on line: %d with code_file_name: %s\n%s" % (code_num, line_num, code_file_name, line))
+                   snipping = True
+                   snip_lines = []
+           else:
+               if end_snipping(line):
+                   snipping = False
+                   codes[code_num] = (code_file_name, '\n'.join(snip_lines))
+                   Log.debug("Closed", code_num, "with", len(snip_lines), "lines on line", line_num)
+               elif line == '\\\n':
+                   snip_lines.append("")
+                   Log.debug("Added an empty new line.")
+               elif line.isspace():
+                   # prepend it to the next line printed
+                   prepending = line[:-1]
+                   Log.debug("Set prepending: '%s'" % prepending)
+               else:
+                   new_line = consume_escapes(line)
+                   Log.debug("New_line now = '%s'" % new_line)
+                   if new_line:
+                       if prepending:
+                           new_line = prepending + new_line
+                           Log.debug("Prepended '%s'" % prepending)
+                           prepending = None
+                       snip_lines.append(new_line.rstrip())
+                       Log.debug("Added new_line: '%s'" % new_line)
 
     return codes
 
@@ -123,24 +118,25 @@ def parse_rtf(file_name):
 def pickle_codes(codes, pickleFile_name):
     pickled = cPickle.dumps(codes, protocol=2)
 
-    try:
-        file = open(pickleFile_name, 'w')
+    with open(pickleFile_name, 'w') as file:
         file.writelines(pickled)
-    except:
-        Log.critical("Unable to create", pickleFile_name)
-
-    file.close()
 
 #######################################################################################
 if __name__ == '__main__':
     import logging
-    Log = utils.get_logger('log', logging.INFO)
+    import utils
+    
+    Log = utils.get_logger('log', logging.DEBUG)
 
-    file_name = sys.argv[1]
-    pFile_name = os.path.join(U_REVISION, os.path.split(file_name)[1] + ".pkl")
-    codes = parse_rtf(file_name)
-    pickle_codes(codes, pFile_name)
-
+    try:
+       file_name = sys.argv[1]
+       pFile_name = os.path.join(U_REVISION, os.path.split(file_name)[1] + ".pkl")
+       codes = parse_rtf(file_name)
+       pickle_codes(codes, pFile_name)
+    except (IndexError, IOError):
+       Log.critical("usage: extractor.py v1.0/TXT.rtfd/TXT.rtf")
+       sys.exit(1)
+       
     for code_num in codes:
         (fname, lines) = codes[code_num]
         print code_num, ":", fname, "> ", lines.splitlines()
